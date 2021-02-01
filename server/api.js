876f5542ab17e1386feed2dc574a39e12f4ddf90
Claire Cheng <clairecheng88@gmail.com>
2021-01-31 20:02:47 -0800

    SOCKET STUFF WORKSSSSSSSS

--- server/api.js
+++ server/api.js
@@ -146,42 +146,47 @@ router.post("/removeFromDance", auth.ensureLoggedIn, async (req, res) => {
   const dancer = await Dancer.findOne({_id: req.body.dancer._id});
   let ind = -1;
   for (let i = 0; i < dancer.rosteredDances.length; i++) {
-    if (dancer.rosteredDances[i] === req.body.danceName) {
+    if (dancer.rosteredDances[i].toString() === req.body.danceName.toString()) {
       ind = i;
       break;
     }
   }
-  const tempList = [...dancer.rosteredDances.slice(0, ind), ...dancer.rosteredDances.slice(ind+1)];
-  const updatedDancer = await Dancer.findOneAndUpdate(
-    { _id: req.body.dancer._id},
-    { $set: {rosteredDances: tempList}},
-    { new: true}
-  );
-  console.log("updated dancer: " + updatedDancer);
-  let dance = await Dance.findOne({ danceId: req.body.danceId });
-  let ind2 = -1;
-  for (let i = 0; i < dance.members.length; i++) {
-    if (dance.members[i]._id.toString() == req.body.dancer._id.toString()) {
-      ind2 = i;
-      break;
-    }
-  }
-  if (ind2 !== -1) {
-    const tempMembers = [...dance.members.slice(0, ind2), ...dance.members.slice(ind2+1)]
-    await Dance.updateOne(
-      { danceId: req.body.danceId },
-      { $set: {members: tempMembers }}
+  if (ind !== -1) {
+    const tempList = [...dancer.rosteredDances.slice(0, ind), ...dancer.rosteredDances.slice(ind+1)];
+    const updatedDancer = await Dancer.findOneAndUpdate(
+      { _id: req.body.dancer._id},
+      { $set: { rosteredDances: tempList }},
+      { new: true}
     );
-    await updateOtherDancesForRemove(dancer);
-    await socketManager.getIo().emit("removeDancerFromDance", {name: req.body.choreogName, removedDancer: dancer, danceName: req.body.danceName});
-    res.send(updatedDancer);
+    console.log("updated dancer: " + updatedDancer);
+    let dance = await Dance.findOne({ danceId: req.body.danceId });
+    let ind2 = -1;
+    for (let i = 0; i < dance.members.length; i++) {
+      if (dance.members[i]._id.toString() == req.body.dancer._id.toString()) {
+        ind2 = i;
+        break;
+      }
+    }
+    if (ind2 !== -1) {
+      const tempMembers = [...dance.members.slice(0, ind2), ...dance.members.slice(ind2+1)]
+      await Dance.updateOne(
+        { danceId: req.body.danceId },
+        { $set: {members: tempMembers }}
+      );
+      await updateOtherDancesForRemove(dancer, updatedDancer);
+      await socketManager.getIo().emit("removeDancerFromDance", {name: req.body.choreogName, removedDancer: dancer, danceName: req.body.danceName});
+      res.send(updatedDancer);
+    }
+    else {
+      res.send("Already removed");
+    }
   }
   else {
     res.send("Already removed");
   }
 })
 
-async function updateOtherDancesForRemove(oldDancer) {
+async function updateOtherDancesForRemove(oldDancer, updatedDancer) {
   const otherDances = await Dance.find({members: oldDancer});
   for (let i = 0; i < otherDances.length; i++) {
     let ind = -1;
@@ -192,7 +197,7 @@ async function updateOtherDancesForRemove(oldDancer) {
       }
     }
     if (ind !== -1) {
-      let tempList = [...otherDances[i].members.slice(0, ind), ...otherDances[i].members.slice(ind+1)];
+      let tempList = [...otherDances[i].members.slice(0, ind), updatedDancer, ...otherDances[i].members.slice(ind+1)];
       await Dance.findOneAndUpdate(
         { _id: otherDances[i]._id },
         { $set: {members: tempList}},
